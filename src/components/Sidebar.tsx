e98ab36664102d901da5d83ed023dcdeae3ba78a
Matheus Rian <souzamatheusrian@gmail.com>
2023-01-07 11:09:32 -0300

    feat: created component SideBar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { PencilLine  } from 'phosphor-react';
+import Styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={Styles.container}>
+      <img 
+        className={Styles.cover}
+        src="https://images.unsplash.com/photo-1573496358349-d97974b2339b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50" 
+      />
+
+      <div className={Styles.profile}>
+        <img src="https://avatars.githubusercontent.com/u/53922139?v=4" />
+        <strong>Matheus Rian</strong>
+        <span>Front-End Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
