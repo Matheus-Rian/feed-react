02326f8899e96b5bc96d858f4490352367b457ea
Matheus Rian <souzamatheusrian@gmail.com>
2023-01-25 15:54:58 -0300

    feat: adding dynamic posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
 import { Header } from "./components/Header";
 import { Sidebar } from './components/Sidebar';
-import { Post } from "./components/Post";
+import { Post } from './components/Post';
 
 import Styles from './App.module.css';
 import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-01-25 14:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-01-25 10:00:00'),
+  },
+];
 
 function App() {
   return (
@@ -12,8 +43,16 @@ function App() {
       <div className={Styles.container}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,34 +1,60 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { Avatar } from './Avatar';
 import { Commment } from './Comment';
 import Styles from './Post.module.css';
 
-export function Post() {
+type Author = {
+  avatarUrl: string;
+  name: string;
+  role: string;
+};
+
+type Content = {
+  type: string;
+  content: string;
+}
+
+type Props = {
+  author: Author;
+  content: Content[];
+  publishedAt: Date;
+}
+
+export function Post({ author, content, publishedAt }: Props) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
   return (
     <article className={Styles.container}>
       <header>
         <div className={Styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/53922139?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div>
-            <strong>Matheus Rian</strong>
-            <span>Front-End Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='22 de novembro às 08h' dateTime='2022-11-22 08:00:12'>Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={Styles.content}>
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p><a href="#">jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="#">#novoprojeto</a> {' '}
-          <a href="#">#nlw</a> {' '}
-          <a href="#">#rocketseat</a> {' '}
-        </p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p><a href="/#">{line.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={Styles.commentForm}>
