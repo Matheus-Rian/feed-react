29c26531a89160f1891359428833ecb56c4276b7
Matheus Rian <souzamatheusrian@gmail.com>
2023-01-25 11:42:10 -0300

    feat: created component Comment

---
+++ src/components/Comment.tsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import Styles from './Comment.module.css';
+
+export function Commment() {
+  return (
+    <div className={Styles.container}>
+      <img src="https://avatars.githubusercontent.com/u/53922139?v=4" />
+
+      <div className={Styles.commentWrapper}>
+        <div className={Styles.commentBox}>
+          <header>
+            <div className={Styles.authorAndTime}>
+              <strong>Matheus Rian</strong>
+              <time title='22 de novembro às 08h' dateTime='2022-11-22 08:00:12'>Cerca de 1 hora atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Parabéns pelo conteúdo 👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
