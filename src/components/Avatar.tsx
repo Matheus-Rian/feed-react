39fcfe0a6d8967a848b22389c4eb9d31add1dd84
Matheus Rian <souzamatheusrian@gmail.com>
2023-01-25 20:12:30 -0300

    refactor: change types component Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,12 +1,12 @@
+import { ImgHTMLAttributes } from 'react';
 import Styles from './Avatar.module.css';
 
-type Props = {
-  src: string;
+interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-};
+}
 
-export function Avatar({ src, hasBorder = true }: Props) {
+export function Avatar({ hasBorder = true, ...rest }: Props) {
   return (
-    <img className={hasBorder ? Styles.avatarWithBorder : Styles.avatar} src={src} />
+    <img className={hasBorder ? Styles.avatarWithBorder : Styles.avatar} {...rest} />
   );
 }
